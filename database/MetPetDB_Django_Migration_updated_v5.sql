01bcada13ab4c37ec0d1fb0c64c5a91e872e52bf
metpetdb <Sibel@cs.rpi.edu>
2012-10-22 13:59:24 -0300

    Update database/MetPetDB_Django_Migration_updated_v5.sql

--- database/MetPetDB_Django_Migration_updated_v5.sql
+++ database/MetPetDB_Django_Migration_updated_v5.sql
@@ -12,6 +12,15 @@ drop table sample_reference_dup;
 drop table sample_regions_dup;
 
 drop sequence element_mineral_types_dup_seq ;
+drop sequence mineral_relationships_dup_seq ;
+drop sequence oxide_mineral_types_dup_seq ;
+drop sequence chemical_analysis_elements_dup_seq ;
+drop sequence chemical_analysis_oxides_dup_seq ; 
+drop sequence sample_metamorphic_grades_dup_seq ;
+drop sequence sample_metamorphic_regions_dup_seq ;
+drop sequence sample_minerals_dup_seq ;
+drop sequence sample_reference_dup_seq ;
+drop sequence sample_regions_dup_seq ;
 
 -- element_mineral_types
 create table element_mineral_types_dup as (select * from element_mineral_types);
@@ -49,7 +58,7 @@ alter table chemical_analysis_elements_dup add foreign key (element_id) referenc
 
 -- chemical_analysis_oxides
 create table chemical_analysis_oxides_dup as (select * from chemical_analysis_oxides);
-create sequence chemical_analysis_oxides_dup_seq; 
+create sequence chemical_analysis_oxides_dup_seq start 1; 
 alter table chemical_analysis_oxides_dup add column id int;
 update chemical_analysis_oxides_dup set id=nextval('chemical_analysis_oxides_dup_seq');
 alter table chemical_analysis_oxides_dup add primary key (id);
@@ -58,7 +67,7 @@ alter table chemical_analysis_oxides_dup add foreign key (oxide_id) references o
 
 -- sample_metamorphic_grades
 create table sample_metamorphic_grades_dup as (select * from sample_metamorphic_grades);
-create sequence sample_metamorphic_grades_dup_seq;
+create sequence sample_metamorphic_grades_dup_seq start 1;
 alter table sample_metamorphic_grades_dup add column id int;
 update sample_metamorphic_grades_dup set id=nextval('sample_metamorphic_grades_dup_seq');
 alter table sample_metamorphic_grades_dup add primary key (id);
@@ -68,7 +77,7 @@ alter table sample_metamorphic_grades_dup add foreign key (sample_id) references
 
 -- sample_metamorphic_regions
 create table sample_metamorphic_regions_dup as (select * from sample_metamorphic_regions);
-create sequence sample_metamorphic_regions_dup_seq;
+create sequence sample_metamorphic_regions_dup_seq start 1;
 alter table sample_metamorphic_regions_dup add column id int;
 update sample_metamorphic_regions_dup set id=nextval('sample_metamorphic_regions_dup_seq');
 alter table sample_metamorphic_regions_dup add primary key (id);
@@ -77,7 +86,7 @@ alter table sample_metamorphic_regions_dup add foreign key (sample_id) reference
 
 -- sample_minerals
 create table sample_minerals_dup as (select * from sample_minerals);
-create sequence sample_minerals_dup_seq;
+create sequence sample_minerals_dup_seq start 1;
 alter table sample_minerals_dup add column id int;
 update sample_minerals_dup set id=nextval('sample_minerals_dup_seq');
 alter table sample_minerals_dup add primary key (id);
@@ -86,7 +95,7 @@ alter table sample_minerals_dup add foreign key (sample_id) references samples (
 
 -- sample_reference
 create table sample_reference_dup as (select * from sample_reference);
-create sequence sample_reference_dup_seq;
+create sequence sample_reference_dup_seq start 1;
 alter table sample_reference_dup add column id int;
 update sample_reference_dup set id=nextval('sample_reference_dup_seq');
 alter table sample_reference_dup add primary key (id);
@@ -95,7 +104,7 @@ alter table sample_reference_dup add foreign key (sample_id) references samples 
 
 -- sample_regions
 create table sample_regions_dup as (select * from sample_regions);
-create sequence sample_regions_dup_seq;
+create sequence sample_regions_dup_seq start 1;
 alter table sample_regions_dup add column id int;
 update sample_regions_dup set id=nextval('sample_regions_dup_seq');
 alter table sample_regions_dup add primary key (id);
@@ -103,8 +112,24 @@ alter table sample_regions_dup add foreign key (region_id) references regions (r
 alter table sample_regions_dup add foreign key (sample_id) references samples (sample_id) match simple on update no action on delete no action;
 
 
--- grant statements tables
-alter table  sample_regions_dup owner to metpetdb_dev ;
+alter table element_mineral_types_dup owner to metpetdb_dev;
+alter table mineral_relationships_dup owner to metpetdb_dev;
+alter table oxide_mineral_types_dup owner to metpetdb_dev;
+alter table chemical_analysis_elements_dup owner to metpetdb_dev;
+alter table chemical_analysis_oxides_dup owner to metpetdb_dev;
+alter table sample_metamorphic_grades_dup owner to metpetdb_dev;
+alter table sample_metamorphic_regions_dup owner to metpetdb_dev;
+alter table sample_minerals_dup owner to metpetdb_dev;
+alter table sample_reference_dup owner to metpetdb_dev;
+alter table sample_regions_dup owner to metpetdb_dev;
 
--- grant statements for sequences
-grant all PRIVILEGES on sample_regions_dup_seq to metpetdb_dev;
+grant all privileges on element_mineral_types_dup_seq  to metpetdb_dev;
+grant all privileges on mineral_relationships_dup_seq  to metpetdb_dev;
+grant all privileges on oxide_mineral_types_dup_seq  to metpetdb_dev;
+grant all privileges on chemical_analysis_elements_dup_seq  to metpetdb_dev;
+grant all privileges on chemical_analysis_oxides_dup_seq  to metpetdb_dev; 
+grant all privileges on sample_metamorphic_grades_dup_seq  to metpetdb_dev;
+grant all privileges on sample_metamorphic_regions_dup_seq  to metpetdb_dev;
+grant all privileges on sample_minerals_dup_seq  to metpetdb_dev;
+grant all privileges on sample_reference_dup_seq  to metpetdb_dev;
+grant all privileges on sample_regions_dup_seq  to metpetdb_dev;

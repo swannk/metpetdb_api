4aeded1266f45296dcfc9a1caafb8eb1068a88a2
kirangilvaz <kirangilvaz@gmail.com>
2012-10-30 07:58:44 -0700

    Dump updated

--- utility/updateData.py
+++ utility/updateData.py
@@ -3,14 +3,14 @@ import sys
 
 
 def getSamples(filePath):
-        fileObj=open(filePath+'web/samples/samples.json','w')
         response = urllib2.urlopen('http://metpetdb.rpi.edu/metpetdb-py/webservices/samples')
-        fileObj.write(str(response.read()))
+	fileObj=open(filePath+'web/samples/samples.json','w')
+	fileObj.write(str(response.read()))
 
 def getChemicalAnalyses(filePath):
-        fileObj=open(filePath+'web/chemical_analyses/chemical_analyses.json','w')
         response = urllib2.urlopen('http://metpetdb.rpi.edu/metpetdb-py/webservices/chemicalanalyses')
-        fileObj.write(str(response.read()))
+        fileObj=open(filePath+'web/chemical_analyses/chemical_analyses.json','w')
+	fileObj.write(str(response.read()))
 
 
 
@@ -20,6 +20,6 @@ if __name__=="__main__":
 		filePath=sys.argv[1]
 		getSamples(filePath)
         	getChemicalAnalyses(filePath)
-	except:
-		print "Please enter filePath. Eg: /home/user/projectfolder/"
+	except Exception as e:
+		print "Exception :"+str(e)+" Please enter filePath. Eg: /home/user/projectfolder/"
 

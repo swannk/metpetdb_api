deabdcbce527635ecae5b1f4df813bd38b584435
kirangilvaz <kirangilvaz@gmail.com>
2012-12-17 17:14:18 -0800

    Updated

--- webservices/views.py
+++ webservices/views.py
@@ -285,12 +285,12 @@ def getMapJSON(query):
 	while i<resultSetSize:
 		jsonValues={}
 		if ((i+1)!=resultSetSize) and (data[i][0]==data[i+1][0] and data[i][1]==data[i+1][1] and data[i][2]==data[i+1][2] and data[i][3]==data[i+1][3]):
-			jsonValues['id']=str(data[i][0]) 
-			jsonValues['sample_number']=str(data[i][1])
-			jsonValues['rock_type']=str(data[i][2])
-			jsonValues['owner']=str(data[i][3])
-			jsonValues['lat']=str(data[i][5])
-			jsonValues['lon']=str(data[i][6])
+			jsonValues['id']=data[i][0] 
+			jsonValues['sample_number']=data[i][1]
+			jsonValues['rock_type']=data[i][2]
+			jsonValues['owner']=data[i][3]
+			jsonValues['lat']=data[i][5]
+			jsonValues['lon']=data[i][6]
 
 			sample_mineral_list=''
 			while data[i][0]==data[i+1][0] and data[i][1]==data[i+1][1] and data[i][2]==data[i+1][2] and data[i][3]==data[i+1][3]:
@@ -301,18 +301,18 @@ def getMapJSON(query):
 			jsonData.append(jsonValues)
 		else:
 			sample_mineral_list=''
-			jsonValues['id']=str(data[i][0]) 
-			jsonValues['sample_number']=str(data[i][1])
-			jsonValues['rock_type']=str(data[i][2])
-			sample_mineral_list=str(data[i][4])
+			jsonValues['id']=data[i][0]
+			jsonValues['sample_number']=data[i][1]
+			jsonValues['rock_type']=data[i][2]
+			sample_mineral_list=data[i][4]
 			jsonValues['sample_minerals']=sample_mineral_list
-			jsonValues['lat']=str(data[i][5])
-			jsonValues['lon']=str(data[i][6])
+			jsonValues['lat']=data[i][5]
+			jsonValues['lon']=data[i][6]
 			jsonData.append(jsonValues)
 		i=i+1
 	return json.dumps(jsonData)
 
-def getSampleResultsJSON(query):
+def getSampleResults(query):
 	cursor=con.cursor()
 	cursor.execute(query)
 	data=cursor.fetchall()
@@ -323,16 +323,16 @@ def getSampleResultsJSON(query):
 	while i<resultSetSize:
 		
 		if ((i+1)!=resultSetSize) and (data[i][0]==data[i+1][0] and data[i][1]==data[i+1][1] and data[i][2]==data[i+1][2] and data[i][3]==data[i+1][3]):
-			htmlData=htmlData+"<tr><td><a href='http://metpetdb.rpi.edu/metpetweb/#sample/"+str(data[i][0])+"'>"+str(data[i][1])+"</a></td><td>"+str(data[i][2])+"</td>"
+			htmlData=htmlData+"<tr><td><a href='http://metpetdb.rpi.edu/metpetweb/#sample/"+unicode(data[i][0])+"'>"+unicode(data[i][1])+"</a></td><td>"+unicode(data[i][2])+"</td>"
 			sample_mineral_list=''
 			while ((i+1)!=resultSetSize) and (data[i][0]==data[i+1][0] and data[i][1]==data[i+1][1] and data[i][2]==data[i+1][2] and data[i][3]==data[i+1][3]):
-				sample_mineral_list=sample_mineral_list+str(data[i][4])+','
+				sample_mineral_list=sample_mineral_list+unicode(data[i][4])+','
 				i=i+1
 			sample_mineral_list=sample_mineral_list[:len(sample_mineral_list)-1]
-			htmlData=htmlData+'<td>'+str(sample_mineral_list)+'</td><td>'+str(data[i][3])+'</td></tr>'
+			htmlData=htmlData+'<td>'+unicode(sample_mineral_list)+'</td><td>'+unicode(data[i][3])+'</td></tr>'
 			
 		else:
-			htmlData=htmlData+"<tr><td><a href='http://metpetdb.rpi.edu/metpetweb/#sample/"+str(data[i][0])+"'>"+str(data[i][1])+"</a></td><td>"+str(data[i][2])+"</td><td>"+str(data[i][4])+"</td><td>"+str(data[i][3])+"</td></tr>"
+			htmlData=htmlData+"<tr><td><a href='http://metpetdb.rpi.edu/metpetweb/#sample/"+unicode(data[i][0])+"'>"+unicode(data[i][1])+"</a></td><td>"+unicode(data[i][2])+"</td><td>"+unicode(data[i][4])+"</td><td>"+unicode(data[i][3])+"</td></tr>"
 		i=i+1
 	htmlData=htmlData+'</tbody></table>'
 	return htmlData
@@ -409,6 +409,6 @@ def metpetdb(request):
 	elif returntype=='map':
 		return HttpResponse(getMapJSON(str(samples)))
 	else:
-		return HttpResponse(getSampleResultsJSON(str(samples)))
+		return HttpResponse(getSampleResults(str(samples)))
 	
 			

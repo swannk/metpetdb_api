602ce3375f0fe5d397b0b05398eef5da79c8d0b6
Tom Rozanski <rozant89@gmail.com>
2013-02-23 15:40:35 -0500

    Revert "Added tests. Also bringing over sample, subsample, chemical analysis APIs from other repository."
    This reverts commit 2ef3137f5f67f20d7432497b1a3943b6653f2580.

--- metpetdb/settings.py
+++ metpetdb/settings.py
@@ -19,6 +19,7 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -119,11 +120,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'django_nose',
 	'webservices',	
 )
-
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to

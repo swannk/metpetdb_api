2b958d258f50165c3d6647cc957652dba4bd090b
Kevin Norris <nykevin.norris@gmail.com>
2013-06-13 16:14:16 -0400

    Add transaction management to API.
    Works around https://github.com/toastdriven/django-tastypie/issues/85
    
    If a request results in a 2xx or 3xx status code, commit the
    transaction, else roll it back.

--- webservices/tastyapi/resources.py
+++ webservices/tastyapi/resources.py
@@ -1,10 +1,28 @@
+from django.db import transaction
 from tastypie.resources import ModelResource
 from tastypie.constants import ALL, ALL_WITH_RELATIONS
 from tastypie import fields
 from tastypie.authorization import Authorization
 from .models import Sample, RockType
 
-class SampleResource(ModelResource):
+class BaseResource(ModelResource):
+    @transaction.commit_manually
+    def dispatch(self, *args, **kwargs):
+        # Transaction begins immediately, before we get here
+        try:
+            response = super(ModelResource, self).dispatch(*args, **kwargs)
+            keep = (200 <= response.status_code < 400)
+        except:
+            transaction.rollback()
+            raise
+        else:
+            if keep:
+                transaction.commit()
+            else:
+                transaction.rollback()
+            return response
+
+class SampleResource(BaseResource):
     rock_type = fields.ToOneField("webservices.tastyapi.resources.RockTypeResource",
                                   "rock_type")
     class Meta:
@@ -19,7 +37,7 @@ class SampleResource(ModelResource):
                 'rock_type': ALL_WITH_RELATIONS,
                 }
 
-class RockTypeResource(ModelResource):
+class RockTypeResource(BaseResource):
     samples = fields.ToManyField(SampleResource, "sample_set")
     class Meta:
         resource_name = "rock_type"

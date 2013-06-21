5c9f55e9b39ed36ed20bb6edec3eefc87d3ef9a9
Kevin Norris <nykevin.norris@gmail.com>
2013-06-21 15:15:45 -0400

    Further enhance admin.
    * Add User model to admin.
    * Add filtering to Sample, Subsample, and ChemicalAnalysis.
    * Add list columns to same and to User model.

--- tastyapi/admin.py
+++ tastyapi/admin.py
@@ -34,18 +34,53 @@ class ChemicalAnalysisAdminForm(forms.ModelForm):
     class Meta:
         model = models.ChemicalAnalysis
 
+class UserAdminForm(forms.ModelForm):
+    clean_version = clean_version_closure(models.User)
+    class Meta:
+        model = models.User
+
+def is_public(obj):
+    return obj.public_data == u'Y'
+
+is_public.boolean = True
+is_public.admin_order_field = 'public_data'
+
 class SampleAdmin(GeoModelAdmin):
     form = SampleAdminForm
+    def latitude(self, sample):
+        return sample.location.x
+    def longitude(self, sample):
+        return sample.location.y
+    list_display = ('id', 'number', 'rock_type', 'owner',
+                    is_public, 'latitude', 'longitude')
+    list_filter = ('public_data', 'collection_date', 'rock_type', 'owner',
+                   'collector', 'regions')
 
 class SubsampleAdmin(admin.ModelAdmin):
     form = SubsampleAdminForm
+    list_display = ('subsample_id', 'name', 'sample', is_public, 'user',
+                    'subsample_type')
+    list_filter = ('public_data', 'user', 'subsample_type')
 
 class ChemicalAnalysisAdmin(admin.ModelAdmin):
     form = ChemicalAnalysisAdminForm
+    list_display = ('chemical_analysis_id', 'subsample', is_public, 'user')
+    list_filter = ('public_data', 'analysis_method', 'analysis_date',
+                   'mineral', 'user')
+
+class UserAdmin(admin.ModelAdmin):
+    form = UserAdminForm
+    exclude = ('password',)
+    list_display = ('name', 'email', 'is_enabled', 'institution')
+    def is_enabled(self, user):
+        return user.enabled == u'Y'
+    is_enabled.boolean = True
+    is_enabled.admin_order_field = 'enabled'
 
 admin.site.register(models.Sample, SampleAdmin)
 admin.site.register(models.RockType)
 admin.site.register(models.Subsample, SubsampleAdmin)
 admin.site.register(models.SubsampleType)
 admin.site.register(models.ChemicalAnalysis, ChemicalAnalysisAdmin)
-
+admin.site.register(models.Mineral)
+admin.site.register(models.User, UserAdmin)

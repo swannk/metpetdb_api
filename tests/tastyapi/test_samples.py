391e81ca5f5a8cce699c94e9ed95d4c2a1d63d42
Krishna Aradhi <krishna@aradhi.me>
2014-04-05 12:09:33 -0400

    Move the valid_post_data variable to a more convenient location

--- tests/tastyapi/test_samples.py
+++ tests/tastyapi/test_samples.py
@@ -39,6 +39,12 @@ class SampleResourceReadTest(TestSetUp):
                           authentication = credentials, format = 'json')
         self.assertHttpOK(resp)
 
+    def test_some_other_user_can_read_a_public_sample(self):
+        credentials = self.get_credentials(user_id=2)
+        resp = client.get('/tastyapi/v1/sample/1/',
+                          authentication = credentials, format = 'json')
+        self.assertHttpOK(resp)
+
     def test_does_not_find_a_non_existent_sample(self):
         credentials = self.get_credentials()
         resp = client.get('/tastyapi/v1/sample/1000/',
@@ -50,45 +56,45 @@ class SampleResourceReadTest(TestSetUp):
         self.assertHttpUnauthorized(resp)
 
 
+valid_post_data = {
+    'user': '/tastyapi/v1/user/1/',
+    'sesar_number': '14342',
+    'public_data': 'Y',
+    'date_precision': '1',
+    'number': 'NL-67:2005-06290',
+    'rock_type': '/tastyapi/v1/rock_type/16/',
+    'regions': ['Pune', 'Mumbai', 'Pune', 'Hyderabad'],
+    'references': ['2004-061314',
+                   '2004-065494',
+                   '2004-065494'],
+    'metamorphic_grades': ['/tastyapi/v1/metamorphic_grade/1/',
+                           '/tastyapi/v1/metamorphic_grade/2/',
+                           '/tastyapi/v1/metamorphic_grade/3/',
+                           '/tastyapi/v1/metamorphic_grade/4/'],
+    'metamorphic_regions': ['/tastyapi/v1/metamorphic_region/427/',
+                           '/tastyapi/v1/metamorphic_region/428/',
+                           '/tastyapi/v1/metamorphic_region/429/',
+                           '/tastyapi/v1/metamorphic_region/430/'],
+    'minerals': ['/tastyapi/v1/mineral/1/',
+                 '/tastyapi/v1/mineral/2/',
+                 '/tastyapi/v1/mineral/3/',
+                 '/tastyapi/v1/mineral/4/'],
+    'description': 'Created by a test case',
+    'location_error': '2000',
+    'country': 'Brazil',
+    'location_text': 'amfadf',
+    'location': 'POINT (-49.3400382995604971 -16.5187797546387003)'
+}
+
 class SampleResourceCreateTest(TestSetUp):
     fixtures = ['auth_users.json', 'users.json', 'regions.json',
                 'references.json', 'metamorphic_grades.json', 'minerals.json',
                 'rock_types.json', 'metamorphic_regions.json']
 
+
     def test_authorized_user_can_create_a_sample(self):
         nt.assert_equal(Sample.objects.count(), 0)
         credentials = self.get_credentials()
-
-        valid_post_data = {
-            'user': '/tastyapi/v1/user/1/',
-            'sesar_number': '14342',
-            'public_data': 'Y',
-            'date_precision': '1',
-            'number': 'NL-67:2005-06290',
-            'rock_type': '/tastyapi/v1/rock_type/16/',
-            'regions': ['Pune', 'Mumbai', 'Pune', 'Hyderabad'],
-            'references': ['2004-061314',
-                           '2004-065494',
-                           '2004-065494'],
-            'metamorphic_grades': ['/tastyapi/v1/metamorphic_grade/1/',
-                                   '/tastyapi/v1/metamorphic_grade/2/',
-                                   '/tastyapi/v1/metamorphic_grade/3/',
-                                   '/tastyapi/v1/metamorphic_grade/4/'],
-            'metamorphic_regions': ['/tastyapi/v1/metamorphic_region/427/',
-                                   '/tastyapi/v1/metamorphic_region/428/',
-                                   '/tastyapi/v1/metamorphic_region/429/',
-                                   '/tastyapi/v1/metamorphic_region/430/'],
-            'minerals': ['/tastyapi/v1/mineral/1/',
-                         '/tastyapi/v1/mineral/2/',
-                         '/tastyapi/v1/mineral/3/',
-                         '/tastyapi/v1/mineral/4/'],
-            'description': 'Created by a test case',
-            'location_error': '2000',
-            'country': 'Brazil',
-            'location_text': 'amfadf',
-            'location': 'POINT (-49.3400382995604971 -16.5187797546387003)'
-          }
-
         resp = client.post('/tastyapi/v1/sample/', data = valid_post_data,
                            authentication = credentials, format = 'json')
         self.assertHttpCreated(resp)
@@ -281,10 +287,10 @@ class SampleResourceUpdateDeleteTest(TestSetUp):
 #                           authentication=credentials, format='json')
 #         self.assertHttpNotFound(resp)
 
-    # def test_read_sample(self):
-    #     from clientlib.single import read_sample
-    #     resp = read_sample(6336)
-    #     self.assertValidJSONResponse(resp)
+#     # def test_read_sample(self):
+#     #     from clientlib.single import read_sample
+#     #     resp = read_sample(6336)
+#     #     self.assertValidJSONResponse(resp)
 
 
 # class TestModels(object):

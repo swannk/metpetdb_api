1a9f15d0f14aaa3485969149e9b3d1c8c7006c97
Krishna Aradhi <krishna@aradhi.me>
2014-04-12 20:12:16 -0400

    Remove unnecessary package: sites

--- metpetdb/settings.py
+++ metpetdb/settings.py
@@ -131,7 +131,6 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',

881dea556eb408f977d937986a970fe758a7d87f
Krishna Aradhi <krishna@aradhi.me>
2014-04-13 16:57:16 -0400

    Add SQL setup scripts

---
+++ database/setup_sql/3 - ctypes.sql
@@ -0,0 +1,10 @@
+BEGIN;
+CREATE TABLE "django_content_type" (
+    "id" serial NOT NULL PRIMARY KEY,
+    "name" varchar(100) NOT NULL,
+    "app_label" varchar(100) NOT NULL,
+    "model" varchar(100) NOT NULL,
+    UNIQUE ("app_label", "model")
+)
+;
+COMMIT;

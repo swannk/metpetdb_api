a7fee2c2ed3588090befdd098b8ba35fc18b4395
Krishna Aradhi <krishna@aradhi.me>
2014-05-05 19:37:13 -0400

    Import settings from environment variables
    These settings include sensitive information like email server settings and database credentials.

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+import dotenv
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "metpetdb.settings")
+    dotenv.read_dotenv('../env_variables.env')
 
     from django.core.management import execute_from_command_line
 
